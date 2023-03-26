8a9123283664e450f1dc63ae54c350f65c22f412
stevennkeneng <nkeneng.junior@yahoo.fr>
2023-03-26 14:08:51 +0200

    script updated

--- src/setup.php
+++ src/setup.php
@@ -1,5 +1,10 @@
 <?php
 
+$currentDir = __DIR__;
+
+// Traverse up the directory hierarchy until we reach the project root directory
+$projectRoot = dirname(dirname(dirname(dirname($currentDir))));
+
 // Create the fos_http_cache.yaml file with some content
 $fosHttpCacheYaml = <<<'YAML'
 fos_http_cache:
@@ -19,15 +24,15 @@ fos_http_cache:
         hash_cache_ttl: 900
         role_provider: true
 YAML;
-file_put_contents(__DIR__ . '/../../../config/packages/fos_http_cache.yaml', $fosHttpCacheYaml);
+file_put_contents($projectRoot.'/config/packages/fos_http_cache.yaml', $fosHttpCacheYaml);
 
 // Modify the routes.yaml file to add some content at the beginning
 $routesYaml = <<<'YAML'
 user_context_hash:
     path: /_fos_user_context_hash
 YAML;
-file_put_contents(__DIR__ . '/../../../config/routes.yaml', $routesYaml . file_get_contents(__DIR__ . '/../../../config/routes.yaml'));
+file_put_contents($projectRoot.'/config/routes.yaml', $routesYaml . file_get_contents($projectRoot.'/config/routes.yaml'));
 
 // Add the PURGE_SERVER env variable to the .env file
 $purgeServer = 'PURGE_SERVER=http://varnish';
-file_put_contents(__DIR__ . '/../../../.env', $purgeServer . "\n", FILE_APPEND);
+file_put_contents($projectRoot.'/.env', $purgeServer . "\n", FILE_APPEND);

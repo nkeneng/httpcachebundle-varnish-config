88a04460a2eebf25f86debb5b84c991b91ff9b0f
stevennkeneng <nkeneng.junior@yahoo.fr>
2023-03-26 17:39:08 +0200

    script updated to modify package.json

--- src/setup.php
+++ src/setup.php
@@ -24,15 +24,29 @@ fos_http_cache:
         hash_cache_ttl: 900
         role_provider: true
 YAML;
-file_put_contents($projectRoot.'/config/packages/fos_http_cache.yaml', $fosHttpCacheYaml);
+file_put_contents($projectRoot . '/config/packages/fos_http_cache.yaml', $fosHttpCacheYaml);
 
 // Modify the routes.yaml file to add some content at the beginning
 $routesYaml = <<<'YAML'
 user_context_hash:
     path: /_fos_user_context_hash
 YAML;
-file_put_contents($projectRoot.'/config/routes.yaml', $routesYaml ."\n \n". file_get_contents($projectRoot.'/config/routes.yaml'));
+file_put_contents($projectRoot . '/config/routes.yaml', $routesYaml . "\n \n" . file_get_contents($projectRoot . '/config/routes.yaml'));
 
 // Add the PURGE_SERVER env variable to the .env file
 $purgeServer = 'PURGE_SERVER=http://varnish';
-file_put_contents($projectRoot.'/.env', $purgeServer . "\n", FILE_APPEND);
+file_put_contents($projectRoot . '/.env', $purgeServer . "\n", FILE_APPEND);
+
+$packageJson = file_get_contents($projectRoot . '/package.json');
+
+// Decode the JSON data into a PHP array
+$data = json_decode($packageJson, true);
+
+// Add a new script to the end of the scripts block
+$data['scripts']['start'] = 'yarn install && encore dev --watch';
+
+// Encode the modified data back into JSON format
+$newPackageJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+// Write the modified data back to the package.json file
+file_put_contents($projectRoot . '/package.json', $newPackageJson);
